d0f4fa9451fe9fec3de7c867b9c3c28e0d4c287b
Dongyong Li <dongyongli@microsoft.com>
2025-03-04 17:46:39 +0800

    实现左滑关闭弹窗

--- miniprogram/pages/recordListPopup/index.ts
+++ miniprogram/pages/recordListPopup/index.ts
@@ -16,12 +16,39 @@ Component({
     }
   },
 
+  data: {
+    startX: 0, // 记录起始X坐标
+    moveX: 0,  // 记录移动X坐标
+  },
+
   methods: {
     onClose() {
       this.setData({ show: false });
       setTimeout(() => {
         wx.navigateBack();
       }, 300);
-    }
+    },
+
+    onTouchStart(e: WechatMiniprogram.TouchEvent) {
+      this.setData({
+        startX: e.touches[0].pageX,
+      });
+    },
+  
+    // 监听手势移动
+    onTouchMove(e: WechatMiniprogram.TouchEvent) {
+      this.setData({
+        moveX: e.touches[0].pageX,
+      });
+    },
+  
+    // 监听手势结束，判断是否左滑关闭
+    onTouchEnd() {
+      const { startX, moveX } = this.data;
+      console.log(moveX - startX);
+      if (moveX - startX < -50) {  // 左滑超过 50px 就关闭
+        this.setData({ show: false });
+      }
+    },
   }
 });
